ff84edf1be932a6cf18a753e73947c148d9f8777
Kavinsharathy01 <166894740+Kavinsharathy01@users.noreply.github.com>
2024-06-28 20:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function foo() {
+    var firstname = document.getElementById("firstname").value;
+    var middlename = document.getElementById("middlename").value;
+    var lastname = document.getElementById("lastname").value;
+    var dateofbirth = document.getElementById("date of birth").value;
+    var age = document.getElementById("age").value;
+    var marks1 = document.getElementById("marks1").value;   
+    var marks2 = document.getElementById("marks2").value;
+    var marks3 = document.getElementById("marks3").value;
+    var marks4 = document.getElementById("marks4").value;
+    var address = document.getElementById("address").value;
+    var mobile  = document.getElementById("mobile no").value;
+    var email = document.getElementById("email").value;
+    var experience = document.getElementById("experience").value;
+    console.log(firstname);
+    console.log(middlename);
+    console.log(lastname);
+    console.log(dateofbirth);
+    console.log(age);
+    console.log(marks1);
+    console.log(marks2);
+    console.log(marks3);
+    console.log(marks4);
+    console.log(address);
+    console.log(mobile);
+    console.log(email);
+    console.log(experience);
+}
